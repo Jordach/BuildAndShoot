98f3c3848ecf291bc70493510e6a05471e81dd09
Jordan Snelling <jordach.snelling@gmail.com>
2013-03-08 21:13:01 +0000

    redo the lot as i cant decide for shit.

--- src/network.h
+++
@@ -1,9 +0,0 @@
-//Part of the BnS Client.
-#include <iostream>
-
-void network_check();
-
-void network_check()
-{
-	cout << "network.cpp active! \n";
-}
--- src/sound.h
+++
@@ -1,9 +0,0 @@
-//Part of the BnS Client...
-#include <iostream>
-
-void sound_check();
-
-void sound_check()
-{
-	cout << "sound.cpp active! \n";
-}
