4dc0c8c0f92d6fe16d305dc59dbbcbb62cd4f429
Jordan Snelling <jordach.snelling@gmail.com>
2013-03-17 14:28:25 +0000

    add the original AoS sounds as i have to use them as placeholders.

---
+++ respawn.py
@@ -0,0 +1,26 @@
+import pygame, time, os
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
+
+soundobj = pygame.mixer.Sound('sounds/beep2.wav')
+soundobj.play()
+print "INSERT COIN: 3"
+time.sleep(1)
+import os
+def cls():
+    os.system(['clear','cls'][os.name == 'nt'])
+cls()
+soundobj.stop()
+soundobj = pygame.mixer.Sound('sounds/beep2.wav')
+soundobj.play()
+print "INSERT COIN: 2"
+time.sleep(1)
+import os
+def cls():
+    os.system(['clear','cls'][os.name == 'nt'])
+cls()
+soundobj.stop()
+soundobj = pygame.mixer.Sound('sounds/beep1.wav')
+soundobj.play()
+print "INSERT COIN: 1"
+time.sleep(1)
+soundobj.stop()
