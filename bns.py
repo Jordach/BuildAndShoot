8efe487a3f989e552582bd046402575011cae6f0
Jordan Snelling <jordach.snelling@gmail.com>
2013-03-17 15:26:21 +0000

    update title window.

--- bns.py
+++ bns.py
@@ -2,8 +2,8 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Drawing!')
+DISPLAYSURF = pygame.display.set_mode((800, 600), 0, 32)
+pygame.display.set_caption('BuildAndAShoot')
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
